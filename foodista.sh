604c79a25903efa95ac3a9c9e64dddf369de3686
Paolo Castagna <paolo.castagna@gmail.com>
2012-01-17 14:24:19 +0000

    Fix foodista.sh.

--- foodista.sh
+++ foodista.sh
@@ -14,11 +14,16 @@ git clone git://github.com/castagna/foodista.git
 cd /mnt/data/foodista || exit 255
 
 echo "crawling foodista"
-rake publish
+rake cache || exit 225
+rake convert || exit 225
+rake publish || exit 255
 
 echo "uploading result to S3"
 s3cmd --no-progress -P -m application/x-gzip put data.gz \
     s3://${AWS_S3_BUCKET_NAME}/${AWS_S3_PATH}/foodista.gz || exit 255
+
+lupy -u ${SOURCE_URI} -x --upload foodista.gz > meta.ttl
+cat meta.ttl
 
 echo "adding additional build metadata"
 curl --silent -i -X POST -H "Content-Type: text/turtle" -T meta.ttl \
